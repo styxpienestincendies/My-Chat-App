9fbebb4acdb7e0cb2cbbb0b664f43b1ff431c473
styxpienestincendies <137850839+styxpienestincendies@users.noreply.github.com>
2024-01-23 10:02:29 +1100

    Sign In Component

--- my-chat-app/src/components/Navbar.jsx
+++ my-chat-app/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import SignIn from './SignIn'
 import { auth } from '../firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
+
 
 const style = {
     nav: `h-20 flex justify-between items-center p-4`,
@@ -8,11 +10,14 @@ const style = {
     heading: `text-white text-3xl`
 }
 
+
 const Navbar = () => {
     const [user] = useAuthState(auth)
+    console.log(user)
     return (
         <div className={style.nav} style={style.backgroundColor}>
             <h1 className={style.heading}> Chat Application</h1>
+            <SignIn />
         </div>
     )
 }
